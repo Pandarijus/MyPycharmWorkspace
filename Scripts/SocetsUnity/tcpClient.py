0e101e5c0221cc955abc70b183b9fe1eea7d0de7
Pandarijus <61548456+Pandarijus@users.noreply.github.com>
2022-05-14 20:03:55 +0200

    Update tcpClient.py

--- Scripts/SocetsUnity/tcpClient.py
+++ Scripts/SocetsUnity/tcpClient.py
@@ -1,7 +1,7 @@
 import socket
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket.connect(("localhost", 6969))
+clientSocket.connect(("192.168.0.25", 6969))# clientSocket.connect(("localhost", 6969))
 
 file = open("test.jpg", "rb")
 imageBytes = file.read(2048)
